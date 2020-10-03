daec318262ac8e303fc3ade31209541d6c011e2b
harshil0404 <71298192+harshil0404@users.noreply.github.com>
2020-10-03 13:00:09 -0400

    Add files via upload

---
+++ Xirovera.com/productStorage_Xirovera.js
@@ -0,0 +1,193 @@
+let products = [
+    {
+        name : "men BLUE jeans",
+        price : 15,
+        imgSrc : "./images/jeans_B1.jpg",
+        category : "clothes sale",
+        tag : "blue jeans men",
+        inCart : 0,
+        id:"jeans1"
+    },
+    {
+        name : "men navy-blue jeans",
+        price : 20,
+        imgSrc : "./images/jeans_NB1.jpg",
+        category : "clothes",
+        tag : "navy blue men jeans",
+        inCart : 0,
+        id:"jeans2"
+    },
+    {
+        name : "men torn jeans",
+        price : 18,
+        imgSrc : "./images/jeans_TORN1.jpg",
+        category : "clothes sale",
+        tag : "torn blue men jeans",
+        inCart : 0,
+        id:"jeans3"
+    },
+    {
+        name : "men torn black jeans",
+        price : 25,
+        imgSrc : "./images/jeans_TORN2.jpg",
+        category : "clothes",
+        tag : "torn men blue jeans",
+        inCart : 0,
+        id:"jeans4"
+    },
+    {
+        name : "sports Shoes",
+        price : 40,
+        imgSrc : "./images/shoes2.jpeg",
+        category : "shoes",
+        tag : "footwear shoes",
+        inCart : 0,
+        id:"shoes1"
+    },
+    {
+        name : "exercise shoes",
+        price : 35,
+        imgSrc : "./images/shoes3.jpeg",
+        category : "shoes sale",
+        tag : "footwear shoes",
+        inCart : 0,
+        id:"shoes2"
+    },
+    {
+        name : "casual shoes",
+        price : 50,
+        imgSrc : "./images/shoes4.jpeg",
+        category : "shoes",
+        tag : "footwear shoes",
+        inCart : 0,
+        id:"shoes3"
+    },
+    {
+        name : "shock resistant shoes",
+        price : 45,
+        imgSrc : "./images/shoes5.jpeg",
+        category : "sale shoes",
+        tag : "footwear shoes",
+        inCart : 0,
+        id:"shoes4"
+    },
+    {
+        name : "eyewear classic",
+        price : 20,
+        imgSrc : "./images/eye1.jpg",
+        category : "EyeWear",
+        tag : "eyewear spects sale",
+        inCart : 0,
+        id:"eye1"
+    },
+    {
+        name : "eyewear plus",
+        price : 25,
+        imgSrc : "./images/eye3.jpg",
+        category : "EyeWear",
+        tag : "eyewear spects",
+        inCart : 0,
+        id:"eye2"
+    },
+    {
+        name : "eyewear unisex",
+        price : 18,
+        imgSrc : "./images/eye4.jpeg",
+        category : "EyeWear",
+        tag : "eyewear spects sale",
+        inCart : 0,
+        id:"eye3"
+    },
+    {
+        name : "eyewear look+",
+        price : 30,
+        imgSrc : "./images/eye5.jpg",
+        category : "EyeWear",
+        tag : "eyewear spects",
+        inCart : 0,
+        id:"eye4"
+    },
+    {
+        name : "Mobile Phone samsung",
+        price : 550,
+        imgSrc : "./images/mob1.jpg",
+        category : "electronics sale",
+        tag : "mobile electronics phone",
+        inCart : 0,
+        id:"phone1"
+    },
+    {
+        name : "Mobile Phone apple",
+        price : 600,
+        imgSrc : "./images/mob2.jpg",
+        category : "electronics sale",
+        tag : "mobile electronics phone",
+        inCart : 0,
+        id:"phone2"
+    },
+    {
+        name : "Mobile Phone S10",
+        price : 850,
+        imgSrc : "./images/mob3.jpg",
+        category : "electronics",
+        tag : "mobile electronics phone",
+        inCart : 0,
+        id:"phone3"
+        
+    },
+    {
+        name : "Pendrive fast-transfer",
+        price : 6,
+        imgSrc : "./images/pendrive.jpg",
+        category : "electronics pendrive sale",
+        tag : "electronics pendrive",
+        inCart : 0,
+        id:"pendrive1"
+        
+    },
+    {
+        name : "Airpods series-4",
+        price : 180,
+        imgSrc : "./images/airpods.jpg",
+        category : "electronics sale",
+        tag : " ",
+        inCart : 0,
+        id:"airpods1"
+    },
+    {
+        name : "FAST Charger",
+        price : 30,
+        imgSrc : "./images/charger.jpg",
+        category : "electronics",
+        tag : "electronics sale",
+        inCart : 0,
+        id:"charger1"
+    },
+    {
+        name : "Headphone ultra-base",
+        price : 95,
+        imgSrc : "./images/headphones.jpg",
+        category : "electronics",
+        tag : "electronics sale",
+        inCart : 0,
+        id:"headphones1"
+    },
+    {
+        name : "Headphone sound-max",
+        price : 110,
+        imgSrc : "./images/headphones2.jpg",
+        category : "electronics",
+        tag : "electronics headphones",
+        inCart : 0,
+        id:"headphones2"
+    },
+    {
+        name : "Headphone pro",
+        price : 190,
+        imgSrc : "./images/headphones3.jpg",
+        category : "electronics",
+        tag : "electronics headphones",
+        inCart : 0,
+        id:"headphones2"
+    },
+];
